9ac54aa1686a5359545708c809fc4ba47610f180
Antti Tuominen <anjtuo@utu.fi>
2023-07-11 20:57:13 +0300

    Luotu toiminto päivityksen muokkaamiselle, tykkäämiselle ja poistamiselle

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -6,7 +6,7 @@ export const getPosts = () => async (dispatch) => {
 
         dispatch({ type: 'FETCH_ALL', payload: data })
     } catch (error) {
-        console.log(error.message)
+        console.log(error)
     }
 }
 
@@ -17,5 +17,35 @@ export const createPost = (post) => async (dispatch) => {
         dispatch({ type: 'CREATE', payload: data })
     } catch (error) {
         console.log(error.message)
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post)
+
+        dispatch({ type: 'UPDATE', payload: data })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id)
+
+        dispatch({ type: 'DELETE', payload: id})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id)
+
+        dispatch({ type: 'LIKE', payload: data })
+    } catch (error) {
+        console.log(error)
     }
 }
