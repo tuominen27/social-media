396b0880acf8570aa526aeb6295f725830533fb2
Antti Tuominen <anjtuo@utu.fi>
2023-07-12 18:31:18 +0300

    Tehty kirjautuminen ja luotu tunnuksen yhteyttä

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,5 +1,5 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes'
-import * as api from '../api'
+import * as api from '../api/index'
 
 export const getPosts = () => async (dispatch) => {
     try {
