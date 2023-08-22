0b7de0f05059671539f9fcb8f4766316de3696aa
Antti Tuominen <tuominenantti4@gmail.com>
2023-08-22 20:18:59 +0300

    Poistettiin kirjautuminen, ei toimi

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
-import Auth from './components/Auth/Auth'
 
 const App = () => (
     <BrowserRouter>
@@ -12,7 +11,6 @@ const App = () => (
             <Navbar />
             <Switch>
                 <Route path="/" exact component={Home} />
-                <Route path="/auth" exact component={Auth} />
             </Switch>
         </Container>
     </BrowserRouter>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,32 +1,18 @@
 import React from "react"
-import { Link } from 'react-router-dom'
-import { AppBar, Toolbar, Typography, Avatar, Button } from '@material-ui/core'
+import { AppBar, Typography  } from '@material-ui/core'
 
 import useStyles from './styles'
 import parhaat from '../../images/parhaat.png'
 
 const Navbar = () => {
     const classes = useStyles()
-
-    const user = null
     
     return(
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Parhaat tulokset</Typography>
+                <Typography className={classes.heading} variant="h2" align="center">Parhaat tulokset</Typography>
                 <img className={classes.image} src={parhaat} alt="icon" height="60" />
             </div>
-            <Toolbar className={classes.toolbar}>
-                {user ? (
-                    <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
-                    </div>
-                ) : (
-                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign in</Button>
-                )}
-            </Toolbar>
         </AppBar>
     )
 }
