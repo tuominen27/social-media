4f98b8f5b917c149953657b7bc47534297bfe692
Antti Tuominen <tuominenantti4@gmail.com>
2023-08-22 20:32:55 +0300

    Muutoksia, jotta error saatiin pois

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 
 const App = () => (
-    <BrowserRouter>
-        <Container maxWidth="lg">
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-            </Switch>
-        </Container>
-    </BrowserRouter>
+    <Container maxWidth="lg">
+        <Navbar />
+        <Home />
+    </Container>
 )
 
 export default App;
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -15,7 +15,7 @@ const Post = ({ post, setCurrentId }) => {
 
     return(
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <CardMedia component='div' className={classes.media} image={post.selectedFile} title={post.title} />
             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
