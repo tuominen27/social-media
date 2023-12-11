e4b012e13ea27f12096e9b919e99fd9a42a831d8
Antti Tuominen <anjtuo@utu.fi>
2023-12-11 18:39:14 +0200

    Lisättiin kommentteja

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+//Käytetty import syntaksia, koska se on helppokäyttöistä. (lisätty modules package.json)
+
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
